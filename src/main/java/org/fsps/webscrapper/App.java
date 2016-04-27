0b6e503ad8254eea4aedd9566aa666522709bfe7
Filip Sochal <fsochal@gmail.com>
2016-04-27 10:03:05 +0200

    Refactored WebscrapperGUI, moved gui starting to App class.

--- src/main/java/org/fsps/webscrapper/App.java
+++ src/main/java/org/fsps/webscrapper/App.java
@@ -1,9 +1,17 @@
 package org.fsps.webscrapper;
 
-public class App
-{
+import org.fsps.webscrapper.view.WebscrapperGUI;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
     public static void main(String[] args) {
-
-
+    	launch(args);
     }
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		 primaryStage.setScene(new WebscrapperGUI().getScene());
+         primaryStage.show();
+	}
 }
