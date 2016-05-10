a3d3f56d4730da0edcf6aedfb687308763bd8e2b
Filip Sochal <fsochal@gmail.com>
2016-05-10 20:09:08 +0200

    Added implementation of presenter.

---
+++ src/main/java/org/fsps/webscrapper/presenter/SearchEnginePresenter.java
@@ -0,0 +1,31 @@
+package org.fsps.webscrapper.presenter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.fsps.webscrapper.searchingLogic.SearchEngine;
+import org.fsps.webscrapper.searchingLogic.page.WebPage;
+import org.fsps.webscrapper.searchingLogic.parser.WebPageParser;
+import org.fsps.webscrapper.view.SearchForm;
+
+public class SearchEnginePresenter implements SeekPresenter {
+	private SearchEngine searchEngine;
+	private WebPageParser parser;
+	
+	public SearchEnginePresenter(SearchEngine searchEngine, WebPageParser parser) {
+		this.searchEngine = searchEngine;
+		this.parser = parser;
+	}
+	
+	@Override
+	public void findByKeywords( SearchForm sourceView, List<String> keywords, List<String> urls, int searchingLevel) throws IOException {
+		List<WebPage> bunchOfPages = parser.parse(urls);
+		List<WebPage> resultPages = searchEngine.findByKeywords(keywords, bunchOfPages, searchingLevel);
+		List<String> results = new ArrayList<>();
+		for(WebPage currentPage : resultPages) {
+			results.addAll(currentPage.getParagraphsContaining(keywords));
+		}
+		sourceView.actualizeResultSet(results);	
+	}
+}
