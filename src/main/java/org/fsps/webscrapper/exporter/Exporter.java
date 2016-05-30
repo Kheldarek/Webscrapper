e42690bd4d25999604ecbf06072bacd4033085c1
Piotr Sęndrowski <psendrowski@gmail.com>
2016-05-30 23:50:21 +0200

    Added export options

---
+++ src/main/java/org/fsps/webscrapper/exporter/Exporter.java
@@ -0,0 +1,82 @@
+package org.fsps.webscrapper.exporter;
+
+import org.fsps.webscrapper.view.mainWindow.WebscrapperGUI;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by psend on 30.05.2016.
+ */
+public class Exporter
+{
+	List<List<String>> paragraphs;
+	List<String> urls;
+	final static String HTML_BEGIN = "<html><head><title>Exported from Websrapper</title></head><body>";
+	final static String HTML_END = "</body></html>";
+	final static String HTML_PARAGRAPH = "<p>%s</p>";
+	final static String HTML_LINK = "<a href = %s >%s</a>";
+
+	public Exporter(List<List<String>> paragraphs, List<String> urls)
+	{
+		this.paragraphs = paragraphs;
+		this.urls = urls;
+	}
+
+	public void ExportToHTML(String path)
+	{
+		String content = HTML_BEGIN;
+		File file = new File(path);
+		for (int i = 0; i < urls.size(); i++)
+		{
+			content = content + String.format(HTML_LINK, urls.get(i) + "\n\n");
+			for (String tmp : paragraphs.get(i))
+			{
+				content += String.format(HTML_PARAGRAPH, tmp + "\n\n");
+			}
+		}
+
+		content += HTML_END;
+		try
+		{
+			FileWriter fileWriter = null;
+			fileWriter = new FileWriter(file);
+			fileWriter.write(content);
+			fileWriter.close();
+		} catch (IOException ex)
+		{
+			System.err.println(ex.getMessage());
+		}
+	}
+
+
+	public void ExportToTxt(String path)
+	{
+		File txtFile = new File(path);
+		String content = "";
+		for (int i = 0; i < urls.size(); i++)
+		{
+			content += urls.get(i) + "\n\n";
+			for (String tmp : paragraphs.get(i))
+			{
+				content += tmp + "\n\n";
+			}
+		}
+		try
+		{
+			FileWriter fileWriter = null;
+			fileWriter = new FileWriter(txtFile);
+			fileWriter.write(content);
+			fileWriter.close();
+		} catch (IOException ex)
+		{
+			System.err.println(ex.getMessage());
+		}
+
+
+	}
+
+
+}
--- src/main/java/org/fsps/webscrapper/view/mainWindow/WebscrapperGUI.java
+++ src/main/java/org/fsps/webscrapper/view/mainWindow/WebscrapperGUI.java
@@ -15,6 +15,7 @@ import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import org.fsps.webscrapper.exporter.Exporter;
 import org.fsps.webscrapper.presenter.SearchEnginePresenter;
 import org.fsps.webscrapper.searchingLogic.BasicSearchEngine;
 import org.fsps.webscrapper.searchingLogic.parser.JsoupParser;
@@ -25,50 +26,53 @@ import org.fsps.webscrapper.view.SearchForm;
 public class WebscrapperGUI implements SearchForm
 {
 	private Scene scene;
-    private Button searchBtn;
-    public TextField  urls,keywords;
-    public TextArea results;
-    private VBox vbox;
-    private MenuBar menu;
-    private Menu file,edit,help;
-    private SeekPresenter presenter;
-	private int depth=2;
+	private Button searchBtn;
+	public TextField urls, keywords;
+	public TextArea results;
+	private VBox vbox;
+	private MenuBar menu;
+	private Menu file, edit, help;
+	private SeekPresenter presenter;
+	private int depth = 2;
+	private List<List<String>> paragraphsList;
+	private List<String> urlsList;
+	private Exporter exporter;
 
 
-	
-	public WebscrapperGUI() throws IOException {
+	public WebscrapperGUI() throws IOException
+	{
 		BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("webscrapperGU.fxml"));
-		scene = new Scene(root, 800,600);
-        searchBtn = (Button) root.getBottom();
-        vbox = (VBox)root.getTop();
-        urls = (TextField)vbox.getChildren().get(1);
-        keywords = (TextField)vbox.getChildren().get(2);
-        results = (TextArea)root.getCenter();
-        results.setEditable(false);
-        results.setWrapText(true);
-        menu = (MenuBar) vbox.getChildren().get(0);
-        file = menu.getMenus().get(0);
-        edit = menu.getMenus().get(1);
-        help = menu.getMenus().get(2);
+		scene = new Scene(root, 800, 600);
+		searchBtn = (Button) root.getBottom();
+		vbox = (VBox) root.getTop();
+		urls = (TextField) vbox.getChildren().get(1);
+		keywords = (TextField) vbox.getChildren().get(2);
+		results = (TextArea) root.getCenter();
+		results.setEditable(false);
+		results.setWrapText(true);
+		menu = (MenuBar) vbox.getChildren().get(0);
+		file = menu.getMenus().get(0);
+		edit = menu.getMenus().get(1);
+		help = menu.getMenus().get(2);
 		SetEvents();
 
-    }
+	}
 
 	private void ChangeDepth()
 	{
-		TextInputDialog dialog = new TextInputDialog(depth+"");
+		TextInputDialog dialog = new TextInputDialog(depth + "");
 		dialog.setTitle("Change Search Depth");
 		dialog.setHeaderText("How deep do You want to search?");
 		dialog.setContentText("Enter depth level");
 
 // Traditional way to get the response value.
 		Optional<String> result = dialog.showAndWait();
-		if (result.isPresent()){
+		if (result.isPresent())
+		{
 			try
 			{
 				depth = Integer.parseInt(result.get());
-			}
-			catch(NumberFormatException e)
+			} catch (NumberFormatException e)
 			{
 				depth = 2;
 			}
@@ -80,129 +84,142 @@ public class WebscrapperGUI implements SearchForm
 	{
 		WebscrapperGUI mainwindow = this;
 
-		file.getItems().get(0).setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				keywords.setText("");
-				FileSelector root= new FileSelector(mainwindow,false);
-				Stage stage = new Stage();
-				root.start(stage);
-			}
+		file.getItems().get(0).setOnAction(event -> {
+			keywords.setText("");
+			FileSelector root = new FileSelector(mainwindow, false);
+			Stage stage = new Stage();
+			root.start(stage);
 		});
-		file.getItems().get(1).setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				urls.setText("");
-				FileSelector root= new FileSelector(mainwindow,true);
-				Stage stage = new Stage();
-				root.start(stage);
-			}
+		file.getItems().get(1).setOnAction(event -> {
+			urls.setText("");
+			FileSelector root = new FileSelector(mainwindow, true);
+			Stage stage = new Stage();
+			root.start(stage);
 		});
 
-		file.getItems().get(2).setOnAction(new EventHandler<ActionEvent>()
-		{
-			@Override
-			public void handle(ActionEvent event)
+		file.getItems().get(2).setOnAction(event -> Platform.exit());
+
+		edit.getItems().get(0).setOnAction(event -> {
+			urls.setText("");
+			keywords.setText("");
+			results.setText("");
+		});
+		edit.getItems().get(1).setOnAction(event -> ChangeDepth());
+		help.getItems().get(0).setOnAction(event -> {
+			Alert alert = new Alert(Alert.AlertType.INFORMATION);
+			alert.setTitle("About");
+			alert.setHeaderText("Webscrapper");
+			alert.setContentText("Metasearch Engine developed by Piotr Sendrowski and Filip Sochal");
+			alert.showAndWait();
+		});
+
+
+
+		searchBtn.setOnAction(event -> {
+			presenter = new SearchEnginePresenter(new BasicSearchEngine(), new JsoupParser());
+			try
 			{
-				Platform.exit();
-			}
-		});
+				presenter.findByKeywords(mainwindow, CreateKeywordsList(), CreateUrlList(), depth);
 
-		edit.getItems().get(0).setOnAction(new EventHandler<ActionEvent>()
-		{
-			@Override
-			public void handle(ActionEvent event)
+
+				//Search
+			} catch (IOException e)
 			{
-				urls.setText("");
-				keywords.setText("");
-				results.setText("");
+				e.printStackTrace();
 			}
+
 		});
-		edit.getItems().get(1).setOnAction(new EventHandler<ActionEvent>()
-		{
-			@Override
-			public void handle(ActionEvent event)
+	}
+	private void SetExportEvents()
+	{
+
+		edit.getItems().get(2).setOnAction(event -> {
+			String path="exportTXT";
+			TextInputDialog dialog = new TextInputDialog();
+			dialog.setTitle("Choose file path");
+			dialog.setHeaderText("Where do You want to export file?");
+			dialog.setContentText("exportTXT");
+
+			Optional<String> result = dialog.showAndWait();
+			if (result.isPresent())
 			{
-				ChangeDepth();
+				path = result.get();
 			}
+
+			exporter.ExportToTxt(path);
 		});
-		help.getItems().get(0).setOnAction(new EventHandler<ActionEvent>()
-		{
-			@Override
-			public void handle(ActionEvent event)
+
+		edit.getItems().get(3).setOnAction(event -> {
+
+			String path="exportHTML";
+			TextInputDialog dialog = new TextInputDialog();
+			dialog.setTitle("Choose file path");
+			dialog.setHeaderText("Where do You want to export file?");
+			dialog.setContentText("exportHTML");
+
+			Optional<String> result = dialog.showAndWait();
+			if (result.isPresent())
 			{
-				Alert alert = new Alert(Alert.AlertType.INFORMATION);
-				alert.setTitle("About");
-				alert.setHeaderText("Webscrapper");
-				alert.setContentText("Metasearch Engine developed by Piotr Sendrowski and Filip Sochal");
-				alert.showAndWait();
+					path = result.get();
 			}
+			exporter.ExportToHTML(path);
 		});
-
-		searchBtn.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				presenter = new SearchEnginePresenter(new BasicSearchEngine(),new JsoupParser());
-				try
-				{
-					presenter.findByKeywords(mainwindow,CreateKeywordsList(),CreateUrlList(),depth);
-
-
-					//Search
-				}
-				catch (IOException e)
-				{
-					e.printStackTrace();
-				}
-
-			}});
 	}
 
-	public Scene getScene() {
+	public Scene getScene()
+	{
 		return scene;
 	}
-    
-    @Override
-    public List<String> getUrls() {
-        return null;
-    }
-
-    @Override
-    public void actualizeResultSet(List<List<String>> results, Collection<String> urls)
-    {
-        ShowResults(results,(List<String>)urls);
-    }
-
-    private void ShowResults(List<List<String>> res, List<String> urls)
-    {
-        results.setText("");
-		String res_to_show = new String();
-        ArrayList<String> tmp = (ArrayList<String>) urls;
-        for (Collection<String> x:res)
-        {
-            res_to_show += tmp.get(res.indexOf(x)) + "\n";
-            for(String y : x)
-                res_to_show += y + "\n";
-            res_to_show += "\n";
-        }
-        results.setText(res_to_show);
-    }
-
-    private List<String> CreateKeywordsList()
-    {
-        String rawKeywords = keywords.getText();
-        return Arrays.asList(rawKeywords.split("[;]"));
-
-    }
-
-    private List<String> CreateUrlList()
-    {
-        return Arrays.asList(urls.getText().split("[;]"));
-
-    }
 
 	@Override
-	public void setPresenter(SeekPresenter presenter) {
+	public List<String> getUrls()
+	{
+		return null;
+	}
+
+	@Override
+	public void actualizeResultSet(List<List<String>> results, Collection<String> urls)
+	{
+
+		paragraphsList = results;
+		urlsList = (List<String>)urls;
+		exporter = new Exporter(paragraphsList,urlsList);
+		SetExportEvents();
+		ShowResults(results, (List<String>) urls);
+
+	}
+
+	private void ShowResults(List<List<String>> res, List<String> urls)
+	{
+		results.setText("");
+		String res_to_show = new String();
+		ArrayList<String> tmp = (ArrayList<String>) urls;
+		for (Collection<String> x : res)
+		{
+			res_to_show += tmp.get(res.indexOf(x)) + "\n";
+			for (String y : x)
+				res_to_show += y + "\n";
+			res_to_show += "\n";
+		}
+		results.setText(res_to_show);
+	}
+
+	private List<String> CreateKeywordsList()
+	{
+		String rawKeywords = keywords.getText();
+		return Arrays.asList(rawKeywords.split("[;]"));
+
+	}
+
+	private List<String> CreateUrlList()
+	{
+		return Arrays.asList(urls.getText().split("[;]"));
+
+	}
+
+	@Override
+	public void setPresenter(SeekPresenter presenter)
+	{
 		this.presenter = presenter;
 	}
 
